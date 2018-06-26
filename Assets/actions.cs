e7b66e5ae32d439711bd43ea5c41087231a33949
VasilyBulgakov <vasiliy.bulgakov@brainymx.tech>
2018-06-26 13:35:48 +0700

    1

---
+++ Assets/actions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class actions : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if( Input.touchCount > 0 )
+		{
+			Touch touch = Input.GetTouch(0);
+			if(touch.phase == TouchPhase.Moved)
+			{
+				var delta = touch.deltaPosition;
+				transform.Rotate(Vector3.up, delta.x);
+			}
+		}
+		
+	}
+}
